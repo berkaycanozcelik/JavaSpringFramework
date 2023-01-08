0cb65a94b45da0ecf4ffdd9142aa4cc7ea404d5a
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-01-08 21:47:12 +0100

    added fortunes via properties file & get them randomly via @value annotation

--- src/berkay/springdemo/HappyFortuneService.java
+++ src/berkay/springdemo/HappyFortuneService.java
@@ -1,12 +1,21 @@
 package berkay.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
 
 @Component
 public class HappyFortuneService implements FortuneService{
+
+    @Value("${foo.fortunes}")
+    private String[] fortunes;
+
     @Override
     public String getFortune() {
-        return "Today is your lucky day";
+        return fortunes[(int) (Math.random() * (fortunes.length - 1))];
     }
 }
