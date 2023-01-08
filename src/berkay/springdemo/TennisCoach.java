a9251a9c6cfc3d6392be24bc7e2a6d5862a07481
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-01-08 20:51:01 +0100

    added setter injection

--- src/berkay/springdemo/TennisCoach.java
+++ src/berkay/springdemo/TennisCoach.java
@@ -6,12 +6,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach{
 
-    @Autowired
     private FortuneService fortuneService;
-
+    //Constructor Injection
+    @Autowired
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
+
+    //Setter Injection
+    //    @Autowired
+    //    public void setFortuneService(FortuneService fortuneService) {
+    //        this.fortuneService = fortuneService;
+    //    }
 
     @Override
     public String getDailyWorkout() {
